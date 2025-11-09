73bf75d2f7fd206c92fe10099c9de9e0b40d74ef
zfzfg <163775060+zfzfg@users.noreply.github.com>
2025-11-09 04:21:15 +0100

    Delete src directory

--- src/main/java/de/zfzfg/eventplugin/util/ColorUtil.java
+++
@@ -1,8 +0,0 @@
-package de.zfzfg.eventplugin.util;
-
-import de.zfzfg.core.util.TextUtil;
-
-public class ColorUtil {
-    public static String color(String text) { return TextUtil.color(text); }
-    public static String stripColor(String text) { return TextUtil.strip(text); }
-}
